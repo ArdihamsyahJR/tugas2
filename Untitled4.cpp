7367c5f461770b282eae8306f365dd77791a1bb4
willyardi <32383340+ArdihamsyahJR@users.noreply.github.com>
2017-10-23 12:47:46 +0700

    menambahkan variable

--- Untitled4.cpp
+++ Untitled4.cpp
@@ -6,5 +6,6 @@ int main(){
 	double lantaiP = 193*1215;
 	double lantaibox = lantaiP*8/1000000;
 	cout <<"Luas Rumah 			: "<<luasrumah<<" m2" <<endl;
-	cout <<"Luas 1 box parket	: "<<lantaibox<<" m2" <<endl; 
+	cout <<"Luas 1 box parket	: "<<lantaibox<<" m2" <<endl;
+	double jumlahBoxP,biayaP,biayaPG,totalB; 
 }
